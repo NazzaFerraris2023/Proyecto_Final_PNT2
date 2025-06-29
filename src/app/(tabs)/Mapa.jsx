d726dea545933ddd155b18ac0c791ca366a60d30
galolato23 <galolato2003@gmail.com>
2025-06-28 22:01:26 -0300

    mapa terminado

--- src/app/(tabs)/Mapa.jsx
+++ src/app/(tabs)/Mapa.jsx
@@ -5,74 +5,8 @@
 
 
 
-  const veterinarias = [
-    {
-      id:'1',
-      title: 'Veterinaria',
-      description: 'Vete belgrano',
-      coordenadas: {latitude: -38.5859, longitude: -38.4186}
-    },
-    {
-      id:'2',
-      title: 'Veterinaria 2',
-      description: '9 a 14:30 hs',
-      coordenadas :{latitude: -34.668574, longitude:-58.496986}
-    }
+  
 
-                              
-// -58	388311,-34	626081;MOVIL;2018-08-10;8:15 a 14:30 hs ;Pavon y Virrey Cevallos;Plaza Garay;Constitución;1;Atenciones clínicas y turnos de castración
-// -58	406886;-34	625913;MOVIL;2018-08-10;8:15 a 14:30 hs ;La Rioja y Cochabamba  ;Plaza Martin Fierro;San Cristobal;3;Atenciones clínicas y turnos de castración
-// -58	474991;-34	645153;MOVIL;2018-08-10;8:15 a 14:30 hs ;Fco. Bilbao y Lacarra  ;Parque Avellaneda;Parque Avellaneda;9;Atenciones clínicas y turnos de castración
-// -58	492992;-34	605509;MOVIL;2018-08-11;10:30 a 17:30 hs;Cuenca 2500;Plaza Aristobulo del Valle;Villa del Parque;11;Atenciones clínicas y turnos de castración                                                         
-// -58	419934;-34	592039;MOVIL;2018-08-11;10:30 a 17:30 hs;Costa Rica 3500 ;Plaza Unidad Latinoamericana ;Palermo;14;Atenciones clínicas y turnos de castración                                                         
-// -58	492992;-34	605509;MOVIL;2018-08-12;10:30 a 17:30 hs;Cuenca 2500;Plaza Aristobulo del Valle ;Villa del Parque;11;Atenciones clínicas y turnos de castración                                                         
-// -58	419934;-34	592039;MOVIL;2018-08-12;10:30 a 17:30 hs;Costa Rica 3500;Plaza Unidad Latinoamericana ;Palermo;14;Atenciones clínicas y turnos de castración                                                         
-// -58	397291;-34	588025;MOVIL;2018-08-17;8:15 a 14:30 hs;Av. Las Heras y Av. Pueyrredon    ;Plaza Tte. Gral. Emilio Mitre;Recoleta;2;Atenciones clínicas y turnos de castración
-// -58	456678;-34	621817;MOVIL;2018-08-17;8:15 a 14:30 hs;Av. Avellaneda y Donato Alvarez;Plaza Angel Gris;Flores;7;Atenciones clínicas y turnos de castración
-// -58	479826;-34	550748;MOVIL;2018-08-17;8:15 a 14:30 hs;García del Río y Melian;Parque Saavedra;Saavedra;12;Atenciones clínicas y turnos de castración
-// -58	475098;-34	688271;MOVIL;2018-08-18;10:30 a 17:30 hs ;Av. Fernandez de la Cruz y Guamaní               ;Plaza Sudamericana ;Villa Riachuelo;8;Atenciones clínicas y turnos de castración                                                         
-// -58	445468;-34	604649;MOVIL;2018-08-18;10:30 a 17:30 hs ;Olaya y Antesana;Plaza Benito Nazar ;Villa Crespo;15;Atenciones clínicas y turnos de castración                                                         
-// -58	475098;-34	688271;MOVIL;2018-08-19;10:30 a 17:30 hs ;Av. Fernandez de la Cruz y Guamaní;Plaza Sudamericana ;Villa Riachuelo;8;Atenciones clínicas y turnos de castración                                                         
-// -58	445468;-34	604649;MOVIL;2018-08-19;10:30 a 17:30 hs ;Olaya y Antesana;Plaza Benito Nazar;Villa Crespo;15;Atenciones clínicas y turnos de castración                                                         
-// -58	369577;-34	626455;MOVIL;2018-08-20;10:30 a 17:30 hs ;Defensa y Brasil;Parque Lezama;San Telmo;1;Atenciones clínicas y desparasitaciones 
-// -58	407732;-34	625827;MOVIL;2018-08-20;10:30 a 17:30 hs ;La Rioja y Cochabamba;Plaza Martin Fierro;San Cristobal;3;Atenciones clínicas y desparasitaciones 
-// -58	506506;-34	6554;MOVIL;2018-08-20;10:30 a 17:30 hs ;Av. Juan B. Alberti y Cafayate;Plaza Salaberry;Mataderos;9;Atenciones clínicas y desparasitaciones 
-// -58	487768;-34	559583;MOVIL;2018-08-20;10:30 a 17:30 hs ;Crisólogo Larralde e/ Machain y Mariano Acha;Plaza Alberdi;Saavedra;12;Atenciones clínicas y desparasitaciones 
-
-
-
-
-
-
-
-
-
-
-  ]
-function decodePolyline(t, e) {
-    let points = [];
-    let index = 0, lat = 0, lng = 0;
-    while (index < t.length) {
-      let b, shift = 0, result = 0;
-      do {
-        b = t.charCodeAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-      let dlat = ((result & 1) ? ~(result >> 1) : (result >> 1));
-      lat += dlat;
-      shift = 0; result = 0;
-      do {
-        b = t.charCodeAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-      let dlng = ((result & 1) ? ~(result >> 1) : (result >> 1));
-      lng += dlng;
-      points.push({ latitude: lat / 1e5, longitude: lng / 1e5 });
-    }
-    return points;
-  }
 
 const GEO_API_KEY = '490f91cd29304c21bc363c0e5896385e';
 
@@ -109,7 +43,7 @@ const GEO_API_KEY = '490f91cd29304c21bc363c0e5896385e';
       })
     }
     cargarLocation()
-
+   
   },[])
 
 
@@ -152,38 +86,20 @@ const GEO_API_KEY = '490f91cd29304c21bc363c0e5896385e';
       }
     }
        useEffect(() => {
-        fetchRoute();
-       },[])
-
-     const fetchRoute = async () => {
-        console.log('paso 1', location)
-        if(!location) return;
-         console.log('paso 2', location)
-        setLoadingRoute(true)
-        setVets([]);
+         const cargarVeterinarias = async () => {
     
-
-        const originStr = `${location.latitude},${location.longitude}`
-
-        const url = `https://api.geoapify.com/v2/geocode/reverse?lat=${location.latitude}&lon=${location.longitude}&apiKey=${GEO_API_KEY} `;
-       
-        try {
-            const resp = await fetch(url, "GET")
-            const data = await resp.json();
-            setVets(data.features)
-            // console.log('data', data)
-            // if(data.routes && data.routes.length){
-            //    const points =  decodePolyline(data.routes[0].overview_polyline.points)
-            //    setRouteCoords(points)
-            // }
-            
-        } catch (error) {
-            setErrorMsg('Fallo en generar la ruta')
-        }
-        setLoadingRoute(false)
-        setSelectedRoute(false)
-        setModalVisible(false)
+      
+      const url =  `https://api.geoapify.com/v2/places?categories=pet.veterinary&filter=circle:${location.longitude},${location.latitude},5000&bias=proximity:${location.longitude},${location.latitude}&limit=20&apiKey=${GEO_API_KEY}`;
+     
+      let resp = await fetch(url)
+      let json = await resp.json()
+   
+      setVets(json.features)
     }
+    
+    cargarVeterinarias()
+
+  }, []);
 
 
 
@@ -225,7 +141,8 @@ const GEO_API_KEY = '490f91cd29304c21bc363c0e5896385e';
           showsCompass              
           onRegionChangeComplete={setMapRegion}
           >
-          {vets.map((v, i) => (
+
+      {vets.map((v, i) => (
         <Marker
           key={i}
           coordinate={{
@@ -235,18 +152,8 @@ const GEO_API_KEY = '490f91cd29304c21bc363c0e5896385e';
           title={v.properties.name}
           description={v.properties.formatted}
         />
-      ))
-    }
-          {
-              vets && (
-              <Polyline
-              coordinates= {vets}
-              strokeColor= '#0077AFF'
-              stokeWidht={5}
-              />
-            )
-          }
-          
+   )) }
+         
         </MapView>
 
         )}
